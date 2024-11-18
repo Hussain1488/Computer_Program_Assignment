99a392af93ace6b7bebd97cff242487c77668887
hussain1488 <hedayati1488@gmail.com>
2024-11-18 16:06:19 +0330

    login and register pages with configs and routes has been created.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'GET':
+    return render_template('login.html')
+  
+
+@app.route('/register')
+def register():
+  return render_template('register.html')
 
 if __name__ == "__main__":
   app.run(debug=True)
